6c8381895b0e428faff267396f3e2e628d74a080
Aramyanartur94 <anastasiasemilova2012@yandex.ru>
2020-10-13 22:12:50 +0800

    Add /details/... handler

--- app.rb
+++ app.rb
@@ -42,6 +42,12 @@ get '/new' do
 	erb :new
 end
 
+# вывод информации о посте
+get '/details/:post_id' do
+	post_id = params[:post_id]
+	erb "Displaying information for post with id #{post_id}"
+end
+
 # обработчик post-запроса /new
 # (браузер отправляет данные на сервер)
 post '/new' do
