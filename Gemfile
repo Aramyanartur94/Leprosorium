40bc1c1a96155379821318c068b44deb6298f323
Aramyanartur94 <anastasiasemilova2012@yandex.ru>
2020-10-20 16:44:27 +0800

    Leprosorium update ActiveRecord

--- Gemfile
+++ Gemfile
@@ -2,3 +2,10 @@ source "https://rubygems.org"
 
 gem "sinatra"
 gem "sinatra-contrib"
+gem "sqlite3"
+gem "activerecord"
+gem "sinatra-activerecord"
+gem "rake"
+group :development do
+	gem "tux"
+end
--- app.rb
+++ app.rb
@@ -2,101 +2,41 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
 require 'sqlite3'
 
-def init_db
-	@db = SQLite3::Database.new 'Leprosorium.db'
-	@db.results_as_hash = true
-	return @db
+set :database, { adapter: 'sqlite3', database: 'leprosorium.db' }
+
+class Post < ActiveRecord::Base
 end
 
-# before вызывается каждый раз при перезагрузке любой страницы
+class Comment < ActiveRecord::Base
+end
+
 before do
-	# инициализация БД
-	init_db
-end
-
-configure do
-	# инициализация БД
-	@db = init_db
-	
-	# создает таблицу если таблица не существует
-	@db.execute 'CREATE TABLE IF NOT EXISTS
-	"Posts"
-	(
-		"id"	INTEGER PRIMARY KEY AUTOINCREMENT,
-		"created_date"	DATE,
-		"content"	TEXT
-	)'
-
-	# создает таблицу если таблица не существует
-	@db.execute 'CREATE TABLE IF NOT EXISTS
-	"Comments"
-	(
-		"id"	INTEGER PRIMARY KEY AUTOINCREMENT,
-		"created_date"	DATE,
-		"content"	TEXT,
-		"post_id"	INTEGER
-	)'
+	@posts = Post.all
 end
 
 get '/' do
-	# выбрать список пастов из БЗ
-	@results = @db.execute 'select * from Posts order by id desc'
-	erb :index			
+	erb :index
 end
 
-# обработчик get-запроса /new
-# (браузер получает страницу с сервера)
 get '/new' do
 	erb :new
 end
 
-# вывод информации о посте
-get '/details/:post_id' do
-	# получаем переменную из url'а 
-	post_id = params[:post_id]
-	# получаем список постов
-	# (у нас будет только один пост)
-	results = @db.execute 'select * from Posts where id = ?', [post_id]
-	# выбираем этот один пост в переменную @row
-	@row = results[0]
-
-	# выбираем комментарии для нашего поста
-	@comments = @db.execute 'select * from Comments where post_id = ? order by id', [post_id]
-
-	# возвращаем представление details.erb 
-	erb :details
+get '/posts/:id' do
+	@post = Post.find(params[:id])
+	erb :post
 end
 
-# обработчик post-запроса /new
-# (браузер отправляет данные на сервер)
+
 post '/new' do
-	# получаем переменную из post-запроса
-	content = params[:content]
-
-	if content.length <= 0
-		@error = 'Введите текст поста'
-		return erb :new
-	end
-
-	# сохранение данных в БД
-	@db.execute 'Insert into Posts (content, created_date) values (?, datetime())', [content]
-	# перенаправление на главную страницу
-	redirect to '/'
-end
-
-# обработчик post-запроса /details/...
-# (браузер отправляет данные на сервер, мы их принемаем)
-post '/details/:post_id' do
-	# получаем переменную из url'а 
-	post_id = params[:post_id]
-
-	# получаем переменную из post-запроса
-	content = params[:content]
-
-	@db.execute 'Insert into Comments (content, created_date, post_id) values (?, datetime(), ?)', [content, post_id]
-	
-	# перенаправляем на страницу поста
-	redirect to ('/details/' + post_id)
+	@p = Post.new params[:post]
+	if @p.save
+		erb 'Новый пост добавлен!'
+	else
+		@error = @p.errors.full_messages.first
+		erb :new
+	end	
 end
